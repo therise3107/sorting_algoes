e837d6c4365f89d1142332dfac0465d634ab3523
Lalit Yadav <lyvworks@gmail.com>
2020-08-11 22:34:05 +0530

    optimised merge sort

--- src/merge_sort.cpp
+++ src/merge_sort.cpp
@@ -4,35 +4,69 @@
 
 #include "merge_sort.h"
 
+// Not fast since we are erasing first element of the vector so O(n)
 vector<int> merge(vector<int> left, vector<int> right) {
   vector<int> result;
 
-  while (left.size() || right.size()) {
-    if (left.size() && right.size()) {
-	  if (left.front() < right.front()) {
-		result.push_back(left.front());
-		left.erase(left.begin());
-	  } else {
-		result.push_back(right.front());
-		right.erase(right.begin());
-	  }
-    } else if (left.empty()) {
-	  for (auto i = 0; i < right.size(); ++i)
-		result.push_back(right[i]);
-	  break;
-	} else {
-      for (auto i = 0; i < left.size(); ++i)
-		result.push_back(left[i]);
-	  break;
+  while (!left.empty() || !right.empty()) {
+	 if (left.empty() || right.empty()) {
+		if (left.empty()) {
+		 for (int i : right)
+			result.push_back(i);
+		 break;
+		} else {
+		 for (int i : left)
+			result.push_back(i);
+		 break;
+		}
+	 } else {
+		if (left.front() < right.front()) {
+		 result.push_back(left.front());
+		 left.erase(left.begin());
+		} else {
+		 result.push_back(right.front());
+		 right.erase(right.begin());
+		}
+	 }
 	}
-  }
 
   return result;
 }
 
+// Optimised merge function
+//vector<int> merge(vector<int>& left, vector<int>& right, long &num_inversions) {
+//	vector<int> result;
+//	int l_index {0}, r_index{0};
+//	int l_size {static_cast<int>(left.size())};
+//	int r_size {static_cast<int>(right.size())};
+//
+//	while (l_size > 0 && r_size > 0) {
+//		if (left[l_index] > right[r_index]) {
+//			num_inversions += l_size;
+//			result.push_back(right[r_index++]);
+//			r_size--;
+//		} else {
+//			result.push_back(left[l_index++]);
+//			l_size--;
+//		}
+//	}
+//
+//	if (r_size <= 0) {
+//		for (int i = l_index; i < left.size(); ++i)
+//			result.push_back(left[i]);
+//		return result;
+//	}
+//
+//	for (int i = r_index; i < right.size(); ++i)
+//			result.push_back(right[i]);
+//
+//	return result;
+//}
+
+
 vector<int> merge_sort(vector<int> arr) {
   if (arr.size() <= 1) {
-	return arr;
+		return arr;
   }
 
   int mid = (int)(arr.size() + 1) / 2;
